a0474a69dd51eec8fc611f19e0af03c8d0fdedfd
NguyenTaiPhat <phat.nguyentai231208@gmail.com>
2026-06-07 23:40:24 +0700

    Remove caption from photo overlay in GallerySection
    Removed caption display from photo overlay in GallerySection.

--- src/components/sections/GallerySection.tsx
+++ src/components/sections/GallerySection.tsx
@@ -68,11 +68,6 @@ function ImageCard({
         {/* Overlay */}
         <div className="absolute inset-0 bg-gradient-to-t from-black/60 via-transparent to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300">
           <div className="absolute bottom-0 left-0 right-0 p-4 transform translate-y-2 group-hover:translate-y-0 transition-transform duration-300">
-            {photo.caption && (
-              <p className="text-white text-sm font-medium line-clamp-2 mb-2">
-                {photo.caption}
-              </p>
-            )}
           </div>
         </div>
 
